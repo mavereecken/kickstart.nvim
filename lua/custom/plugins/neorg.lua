a53c2fc7a4cde71292d71816ed3a1529695e7ed0
Matthias Vereecken <matthias.vereecken@uclouvain.be>
2024-04-04 22:39:38 +0200

    Fix Neorg after update

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -1,32 +1,30 @@
 return {
-  {
-    'vhyrro/luarocks.nvim',
-    priority = 1000,
-    config = true,
-  },
-  {
-    'nvim-neorg/neorg',
-    dependencies = { 'luarocks.nvim' },
-    build = ':Neorg sync-parsers',
-    -- tag = "*",
-    lazy = true, -- enable lazy load
-    ft = 'norg', -- lazy load on file type
-    cmd = 'Neorg', -- lazy load on command
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {}, -- Loads default behaviour
-          ['core.concealer'] = {}, -- Adds pretty icons to your documents
-          ['core.dirman'] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = '~/notes',
-                gna = '~/projects/gna/notes/',
-              },
-            },
-          },
-        },
-      }
-    end,
-  },
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+	{
+		"nvim-neorg/neorg",
+		dependencies = { "luarocks.nvim" },
+		-- tag = "*",
+		lazy = false, -- enable lazy load
+		version = "*",
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/notes",
+								gna = "~/projects/gna/notes/",
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
 }
